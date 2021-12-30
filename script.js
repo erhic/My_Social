a96ca087f81b09a6ec3fb3c05a37da2fd6e0881a
erhic <eric.gichovi@student.moringaschool.com>
2021-12-30 18:36:05 +0300

    add header link and badge

---
+++ script.js
@@ -0,0 +1,16 @@
+const searchbg = document.getElementById("search").addEventListener("click", display);
+
+function display() {
+    // `<div class="col-md-4">
+    //             <div id="header_search" class="input-group ">
+    //                 <span style="background-color: white;
+    //                 border-right: 2px solid white;" id="search_input_header1" id="search" class="input-group-text" id="basic-addon1"><i class="bi bi-search"></i></span>
+    //                 <input style="border-left: 2px solid white;
+    //                 border-right: 2px solid white;" id="search_input_header" id="search" type="text" class="form-control" placeholder=" Search for friends,post or video" />
+    //                 <span style="background-color: red;
+    //                 border-left: 2px solid white;"id="search_input_header2" id="search" class="input-group-text" id="basic-addon1"><i class="bi bi-search text-white"></i></span>
+
+    //             </div>
+    //         </div>`
+    alert(123)
+}
