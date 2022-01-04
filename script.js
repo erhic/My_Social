07becd42feed5804d636d73b72272353e856ce09
erhic <eric.gichovi@student.moringaschool.com>
2022-01-04 14:19:13 +0300

    add event listener

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-const searchbg = document.getElementById("search").addEventListener("click", display);
+// const searchbg = document.getElementById("search").addEventListener("click", display);
 
-function display() {
-    // `<div class="col-md-4">
-    //             <div id="header_search" class="input-group ">
-    //                 <span style="background-color: white;
-    //                 border-right: 2px solid white;" id="search_input_header1" id="search" class="input-group-text" id="basic-addon1"><i class="bi bi-search"></i></span>
-    //                 <input style="border-left: 2px solid white;
-    //                 border-right: 2px solid white;" id="search_input_header" id="search" type="text" class="form-control" placeholder=" Search for friends,post or video" />
-    //                 <span style="background-color: red;
-    //                 border-left: 2px solid white;"id="search_input_header2" id="search" class="input-group-text" id="basic-addon1"><i class="bi bi-search text-white"></i></span>
+// function display() {
+//     // `<div class="col-md-4">
+//     //             <div id="header_search" class="input-group ">
+//     //                 <span style="background-color: white;
+//     //                 border-right: 2px solid white;" id="search_input_header1" id="search" class="input-group-text" id="basic-addon1"><i class="bi bi-search"></i></span>
+//     //                 <input style="border-left: 2px solid white;
+//     //                 border-right: 2px solid white;" id="search_input_header" id="search" type="text" class="form-control" placeholder=" Search for friends,post or video" />
+//     //                 <span style="background-color: red;
+//     //                 border-left: 2px solid white;"id="search_input_header2" id="search" class="input-group-text" id="basic-addon1"><i class="bi bi-search text-white"></i></span>
 
-    //             </div>
-    //         </div>`
-    alert(123)
-}
+//     //             </div>
+//     //         </div>`
+//     alert(123)
+// }
+
+// var marks;
+
+// add eventlistener
+let likes = document.querySelector('like').addEventListener('click', tick);
+
+function tick() {
+    // let valuelike = 0;
+    // valuelike + 1;
+    // return valuelike;
+    var k = 20;
+    var j = 30;
+    var c = k + j
+
+    console.log(c)
+    setTimeout(function() {
+        alert("LUNCH TIME NOW");
+        debugger;
+    }, 2000);
+};
